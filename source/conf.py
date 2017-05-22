14bc4b73a30a369eabbe1c25a3a6c5a963ee97ae
Cliff Xuan <cxuan@splunk.com>
2017-05-22 16:23:09 +0800

    fix some issues

--- source/conf.py
+++ source/conf.py
@@ -43,7 +43,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'use python on windows'
+project = u'use python'
 copyright = u'2015, xcgspring'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -176,7 +176,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'usepythononwindowsdoc'
+htmlhelp_basename = 'usepythondoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -196,7 +196,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'usepythononwindows.tex', u'use python on windows Documentation',
+  ('index', 'usepython.tex', u'use python',
    u'xcgspring', 'manual'),
 ]
 
@@ -226,7 +226,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'usepythononwindows', u'use python on windows Documentation',
+    ('index', 'usepython', u'use python',
      [u'xcgspring'], 1)
 ]
 
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'usepythononwindows', u'use python on windows Documentation',
-   u'xcgspring', 'usepythononwindows', 'One line description of project.',
+  ('index', 'usepython', u'use python',
+   u'xcgspring', 'usepython', 'One line description of project.',
    'Miscellaneous'),
 ]
 
--- source/debug.rst
+++ source/debug.rst
@@ -11,22 +11,24 @@ python debug
 pdb
 ====
 
-python自带的调试器,使用``python -m pdb myscript.py``或者``import pdb; pdb.set_trace()``进入调试模式(post-mortem debugging)
+python自带的调试器,使用 ``python -m pdb myscript.py`` 或者 ``import pdb; pdb.set_trace()`` 进入调试模式(post-mortem debugging)
 
 debug python code with gdb
 ==========================
 
 pdb可以解决大部分python调试的需求,但是在一些复杂场景下,需要更强大的工具.
-- 调试正在运行的process
-- 调试可能hng的程序
-- 调试多线程
+ - 调试正在运行的process
+ - 调试可能hng的程序
+ - 调试多线程
 
 参考::
+
   https://wiki.python.org/moin/DebuggingWithGdb
 
 remote debug
 ============
 
 remote debug一般是IDE中提供的功能, 对于pycharm, 参考::
+
   https://www.jetbrains.com/help/pycharm/2017.1/remote-debugging.html#6
 
